e9637b86c37db22ee910cc3b975dbaee6f387926
OMSAH <huncyoms@gmail.com>
2024-03-21 08:36:49 -0400

    commited

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import Navbar from './components/navbar/Navbar'
 import './App.scss'
+import Navbar from './components/navbar/Navbar'
+import Home from './components/home/Home'
 
 export default function App() {
 
   return (
     <div className='app'>
       <Navbar/>
+      <Home/>
     </div>
   )
 }
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./home.scss";
+
+export default function Home() {
+  return (
+    <div id='home'>
+      <div className="container">
+        <div className='home-image'>
+            image
+        </div>
+        <h1>I'm Om Sah. <span>Full Stack Web Developer</span></h1>
+        <p>I am passinate on full stack web developer</p>
+      </div>
+    </div>
+  )
+}
