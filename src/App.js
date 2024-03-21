fa3a6d7d7031e9b6744528366c44e5a8b44424c3
OMSAH <huncyoms@gmail.com>
2024-03-21 04:03:15 -0400

    commited

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, { useState } from 'react'
-import Navbar from './components/navbar/Navbar'
-import Menu from './components/menu/Menu';
+import React from 'react'
 
 export default function App() {
-  const [openMenu, setOpenMenu] = useState(false);
 
   return (
     <div>
-      <Navbar openMenu={openMenu} setOpenMenu={setOpenMenu}/>
-      <Menu openMenu={openMenu} setOpenMenu={setOpenMenu}/>
+      Hello this is 
     </div>
   )
 }
